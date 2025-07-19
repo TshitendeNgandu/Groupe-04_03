c9a2e707b02f082d6045c6340159495e7b821d36
TshitendeNgandu <ryantshitende@gmail.com>
2025-07-20 00:07:45 +0100

    Add files via upload

---
+++ 01_exercice 05.py
@@ -0,0 +1,7 @@
+heures=int(input("heures :"))
+minutes=int(input("minutes :"))
+secondes=int(input("secondes :"))
+
+#durée en secondes 
+durée= heures*3600 + minutes*60 + secondes
+print(f" la durée en secondes est : {durée} secondes.")
